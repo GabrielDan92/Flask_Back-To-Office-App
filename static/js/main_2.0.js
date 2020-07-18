fc2b269fc54b98144d8d1c50cf1b9b3f8b59afe3
Gabriel <gaby.tzu@windowslive.com>
2020-07-18 11:45:39 +0300

    2.0

--- static/js/main_2.0.js
+++ static/js/main_2.0.js
@@ -6,42 +6,30 @@
 // hidden elements that become visible once an action is done on the page
 // eg: when an user wants to add a new employee, the form divs become visible and the original div changes its size
 document.getElementById("userForm").style.display = "none";
-document.getElementsByClassName("date")[0].style.display = "none";
-if (document.getElementById("max")!== null) document.getElementById("max").style.visibility = "hidden";
 if (document.getElementById("safeCheck")!== null) document.getElementById("safeCheck").style.display = "none";
-document.getElementById("validationDate2").style.display = "none";
-
-// create an object to store the month and day from the user
-var date = ""
-var dateObj = {
-    day: "",
-    month: ""
-}
+if (document.getElementById("empCheck")!== null) document.getElementById("empCheck").style.display = "none";
+if (document.getElementById("max")!== null) document.getElementById("max").style.visibility = "hidden";
+if (document.getElementById("empName")!== null) document.getElementById("empName").style.display = "none";
+document.getElementById("validationDate").valueAsDate = new Date();
 
 // make the userform visible upon calling the function and resize several elements in the page
 function visible() {
     document.getElementById("alwaysOnMenu").classList.remove("col-12");
-    document.getElementById("alwaysOnMenu").classList.add("col-5");
+    document.getElementById("alwaysOnMenu").classList.add("col-6");
     document.getElementById("userForm").classList.remove("col-0");
-    document.getElementById("userForm").classList.add("col-7");
+    document.getElementById("userForm").classList.add("col-6");
     document.getElementById("userForm").style.display = "block";
 }
 
 // cancel the above changes and delete any potential values sent to the dateObj object
 function notVisible() {
-    document.getElementById("alwaysOnMenu").classList.remove("col-5");
+    document.getElementById("alwaysOnMenu").classList.remove("col-6");
     document.getElementById("alwaysOnMenu").classList.add("col-12");
-    document.getElementById("userForm").classList.remove("col-7");
+    document.getElementById("userForm").classList.remove("col-6");
     document.getElementById("userForm").classList.add("col-0");
     document.getElementById("userForm").style.display = "none";
-    document.getElementById("month").innerHTML = "Select The Month";
-    document.getElementById("day").innerHTML = "Select The Day";
     document.getElementById("validationName").value = "";
     document.getElementById("validationDepartment").value = "";
-    document.getElementsByClassName("date")[0].value = "";
-    dateObj.month = ""
-    dateObj.day = ""
-    document.getElementsByClassName("date")[0].style.display = "none";
 }
 
 // make the limit form for the employee threshold visible for the user and make sure the span element is hidden
@@ -56,11 +44,11 @@ function setMax() {
 }
 
 // verify the user added value and send a jquery ajax post request
-// only if the value is a positive number, else return a span html element
+// if the value is a positive number, else return a span html element
 function setMaxDone() {
     document.getElementById("max").style.visibility = "hidden";
     var value = Number(document.getElementById("max-text").value)
-
+    
     if (Number.isInteger(value) && value > 0) {
         document.getElementById("safeCheck").style.display = "none";
         var url = "/employeeCount";
@@ -71,47 +59,29 @@ function setMaxDone() {
     } else {
         document.getElementById("safeCheck").style.display = "block";
     }
-
 }
 
-// call the addDate function with the month value
-function visibleMonth(month) {
-    document.getElementById("month").innerHTML = month;
-    addDate(month, visibleMonth)
-}
-
-// call the addDate function with the day value
-function visibleDay(i){
-    document.getElementById("day").innerHTML = i;
-    addDate(i, visibleDay)
-}
-
-// if both the month and day values in the dateObj object are populated
-// add the values in a final date form
-function visibleDate(){
-    document.getElementsByClassName("date")[0].value = date;
-    document.getElementsByClassName("date")[0].style.display = "block";
-    document.getElementById("validationDate2").value = date;
-}
-
-// verifi what function called the addDate() and add the appropiate value in the dateObj object
-function addDate(value, callerFunction) {
-    if (callerFunction == visibleMonth) {
-        dateObj.month = value;
-        console.log("The value " + value + " has been added to the month.")
+function setEmp() {
+    if (document.getElementById("empName").style.display == "block") {
+        document.getElementById("empName").style.display = "none";
+        document.getElementById("empCheck").style.display = "none";
     } else {
-        dateObj.day = value;
-        console.log("The value " + value + " has been added to the day.")
-    }
-
-    if (dateObj.day !== "" && dateObj.month !== "") {
-        date = dateObj.month + "-" + dateObj.day
-        visibleDate()
+        document.getElementById("empName").style.display = "block";
     }
 }
 
+// verify the user added value and send a SQLAlchemy query request
+// if the field is empty, return a span html element
+function setEmpDone() {
+    var value = document.getElementById("max-text").value
+    if (value !== "") {
+        window.location = "individualRemove/" + value;
+    } else {
+        document.getElementById("empCheck").style.display = "block";
+    }
+}
 
-// bootstrap generated function
+// bootstrap validation script
 (function () {
     'use strict';
     window.addEventListener('load', function () {
